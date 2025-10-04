6c35538064361a614f13d300f4b249b309d44b43
DerickVargas7 <derick0787@gmail.com>
2025-10-03 22:02:23 -0400

    Pagina principal de servicios y filtros

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -3,25 +3,41 @@ import prisma from "../../config/prismaClient.js";
 import { googleClient } from "../../config/googleClient.js";
 
 export async function verifyGoogleToken(idToken) {
+
   const ticket = await googleClient.verifyIdToken({
     idToken,
     audience: process.env.GOOGLE_CLIENT_ID
   });
-  return ticket.getPayload(); // {sub, email, name, picture, ...}
+  return ticket.getPayload(); // {informacion del usuario del payload del token de google}
 }
 
 export async function findOrCreateUser(googleUser) {
   const { sub, email, name, picture } = googleUser;
 
-  let user = await prisma.usuarios.findUnique({ where: { googleId: sub } });
+  let user = await prisma.usuario.findUnique({
+    where: { googleId: sub }
+  });
 
   if (!user) {
-    user = await prisma.user.create({
+    user = await prisma.usuario.create({
       data: {
         googleId: sub,
         email,
-        name,
-        picture
+        esActivo: true,
+        es_configurado: false
+      }
+    });
+
+    // perfil basico
+    await prisma.perfil.create({
+      data: {
+        usuarioId: user.id,
+
+        nombreCompleto: name || "",
+
+        fotoUrl: picture || "",
+
+        telefono: "" 
       }
     });
   }
