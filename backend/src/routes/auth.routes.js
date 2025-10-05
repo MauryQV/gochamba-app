9aeda7f38ccb8e709d477e3641eb3fa5dd7ab5d0
Andy Zurita Gonzales <andyzurita36@gmail.com>
2025-10-04 21:58:59 -0400

    validación de datos

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { googleAuth } from "../controllers/auth.controller.js";
+import { register } from "../services/auth.service.js";
 
 const router = Router();
 
 router.post("/auth/google/", googleAuth);
+router.post("/auth/register", register);
 
 export default router;
+
